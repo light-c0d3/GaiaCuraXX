fc052635511b982d2d156a07239038b6d84fc950
malika2470 <malika2470@gmail.com>
2024-09-28 01:49:24 -0400

    part 1 of front end.

--- gaiacura/lib/pages/login_page.dart
+++ gaiacura/lib/pages/login_page.dart
@@ -5,6 +5,60 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(height: 50),
+
+              // welcome back, you've been missed!
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // Username TextField
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue), // Customize color as needed
+                  ),
+                ), // <-- Add this closing parenthesis
+              ),
+
+              // password textfield
+
+              // forgot password?
+
+              // sign in button
+
+              // or continue with
+
+              // google + apple sign in buttons
+
+              // not a member? register now
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
