ba53b66dce5f307453cbc23e3f166d4f55f0658e
snowie0 <rnurkzmz11@gmail.com>
2024-09-28 05:22:28 -0400

    api key added

---
+++ gaiacura/lib/pages/chat_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
+// on main dart home: ChatPage(),
